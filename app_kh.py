6d6caf83b0167564ec741a183d1ef79e18903bd0
atiacarnesia <atiacarnesiatb8@gmail.com>
2025-07-15 07:01:38 +0700

    Update app_kh.py

--- app_kh.py
+++ app_kh.py
@@ -78,7 +78,7 @@ if st.sidebar.button("🔎 Tampilkan Visualisasi"):
         st.stop()
 
     # Wilayah Kalimantan Tengah diperluas
-    lat_min, lat_max = -0.5, 3.0  # Latitude untuk Kalimantan Tengah
+    lat_min, lat_max = -1.5, 3.0  # Latitude untuk Kalimantan Tengah
     lon_min, lon_max = 111.0, 116.5  # Longitude untuk Kalimantan Tengah
     var = var.sel(lat=slice(lat_min, lat_max), lon=slice(lon_min, lon_max))
 
